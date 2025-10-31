5c57f387023e663a3362592835211c566c5ff024
gabriel felipe <gabrielfelipe22707384@gmail.com>
2025-10-31 19:01:03 -0300

    feat: passagem dos paths agora funciona em todos os OS. Agora os registros são salvos em csv (infelizmente imagens não estão inclusas)

--- pythonCV/pythonYOLO.py
+++ pythonCV/pythonYOLO.py
@@ -1,21 +1,23 @@
 from ultralytics import YOLO
 import cv2
 import datetime
-import pandas as pd
-model = YOLO("..\\modelo_YOLO_cigarroV6\\best.pt") #estou utilizando o pytorch
-#futuramente deve-se melhorar essa passagem de caminho do modelo, pois assim só funciona no windows.
+from pathlib import Path
+import registroPlanilha
+
+# Devido o uso da biblioteca pathlib, agora o caminho para os arquivos funcionam em qualquer SO
 
 #   para realizar registro
-registros = [] # essa lista será futuramente usada p registro em planilha via pandas
 ultimo_registro = None
 alerta = False
 cont_registro = 0
-cooldown = 10
+cooldown = 60
+output_pasta = Path("fumantes") 
+nome_planilha = "registrosFumantes.csv"
 
-#   codigo abaixo é para abrir a webcam e rodar o modelo nela
-#webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW) # esse cap_dshow é só pra windows, se trata do video source
-#webcam.set(3,640)
-#   abrir video e rodar o modelo nele:
+output_pasta.mkdir(exist_ok=True) # se a pasta "fumantes" não existir, ela será criada 
+
+model = YOLO((Path("modelo_YOLO_cigarroV6") / "best.pt")) #estou utilizando o pytorch
+
 capVideo = cv2.VideoCapture("fumantes_video.avi")
 while(capVideo.isOpened()):
     dataHoraAtual = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
@@ -28,12 +30,12 @@ while(capVideo.isOpened()):
     fumando_detectado = False
 
     # procura pessoas, cigarros e fumantes em cada frame do vídeo:
-    resultados = model.predict(frame, conf=0.6, iou= 0.3, verbose=False)
+    resultados = model.predict(frame, conf=0.6, iou= 0.2, verbose=False)
 
     # Analisa TODAS as detecções do frame
     for resultado in resultados:
         for caixa in resultado.boxes:
-            classe = model.names[int(caixa.cls[0])]
+            classe = model.names[int(caixa.cls[0])] # retorna pra variavel classe o que foi detectado no frame
             if classe == "Pessoa":
                 pessoa_detectada = True
             elif classe == "Cigarro":
@@ -50,25 +52,29 @@ while(capVideo.isOpened()):
     # se tem fumante, salva o frame em jpg na pasta fumantes, mas verifica umas coisas antes
     if(fumante_detectado):
         agora = datetime.datetime.now()
+        
         # se é o primeiro alerta, salvar sem checagem de cooldown
         if (not alerta):
             ultimo_registro = agora
-            cv2.imwrite(f'fumantes\\fumante_{cont_registro}.jpg', frameAnotado)
+            cv2.imwrite(output_pasta / f'fumante_{cont_registro}.jpg', frameAnotado)
+            registroPlanilha.adicionar_registro(output_pasta / f'fumante_{cont_registro}.jpg', nome_planilha, "cafeteria(placeholder)")
             cont_registro+=1
             alerta = True
         # nao é o primeiro alerta
         else:
             print((agora - ultimo_registro).total_seconds())
             if((agora - ultimo_registro).total_seconds() > cooldown):
-                cv2.imwrite(f'fumantes\\fumante_{cont_registro}.jpg', frameAnotado)
+                cv2.imwrite(output_pasta / f'fumante_{cont_registro}.jpg', frameAnotado)
+                registroPlanilha.adicionar_registro(output_pasta / f'fumante_{cont_registro}.jpg', nome_planilha, "cafeteria(placeholder)")
                 ultimo_registro = agora
                 cont_registro+=1
 
+        
     cv2.putText(frameAnotado, dataHoraAtual, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 250, 250), 2, cv2.LINE_AA)
     cv2.imshow('VideoShow', frameAnotado)
 
 
-    if (cv2.waitKey(1) & 0xFF == ord('q')): # pressionar q para fechar a janela
+    if (cv2.waitKey(1) & 0xFF == ord('q')): # pressionar q para fechar a janela, 0xFF padroniza a entrada da tecla digitada p/ todos SO e versões do openCV
         break
 
 capVideo.release()
---
+++ pythonCV/registroPlanilha.py
@@ -0,0 +1,23 @@
+import datetime
+import pandas as pd
+from pathlib import Path
+registros = []
+def adicionar_registro(imagem_path, nome_planilha, localOcorrido):
+    if not Path(nome_planilha).exists():
+        df = pd.DataFrame(columns=[
+            'data', 'hora', 'imagem', 'local'
+        ])
+        df.to_csv(nome_planilha, index=False)
+        print(f" Planilha criada: {nome_planilha}")
+
+    agora = datetime.datetime.now()
+    registro = {
+        'data': agora.strftime('%d/%m/%Y'),
+        'hora': agora.strftime('%H:%M:%S'),
+        #'imagem': imagem_path,
+        'local': localOcorrido
+    }
+
+    registros.append(registro)
+    df = pd.DataFrame(registros)
+    df.to_csv(nome_planilha, index=False, encoding='utf-8')
